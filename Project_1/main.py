e11b027109f94ddcd874b7c9500d95db86c62a35
Niklas <46137291+NiiklasDEV@users.noreply.github.com>
2021-11-08 16:37:46 +0100

    Update main.py

--- Project_1/main.py
+++ Project_1/main.py
@@ -7,11 +7,11 @@ class Settings(object):
     window_width = 700
     path_file = os.path.dirname(os.path.abspath(__file__))
     path_image = os.path.join(path_file, "images")
-    pacman_size = (25,25)
+    player_size = (25,25)
     size1 = 25
     size2 = 25
     Meteors_size = (size1,size2)
-    title = "Pacman Pygame"
+    title = "player Pygame"
 
 class Background(pygame.sprite.Sprite):
     def __init__(self, filename) -> None:
@@ -30,13 +30,43 @@ class Background(pygame.sprite.Sprite):
 class Player(pygame.sprite.Sprite):
     def __init__(self, filename) -> None:
         super().__init__()
+        self.movex = 0
+        self.movey = 0
+        self.frame = 0
         self.image = pygame.image.load(os.path.join(Settings.path_image, filename)).convert_alpha()
-        self.image = pygame.transform.scale(self.image, Settings.pacman_size)
+        self.image = pygame.transform.scale(self.image, Settings.player_size)
         self.rect = self.image.get_rect()
         self.rect.left = randint(5, Settings.window_width - self.rect.width)
         self.rect.top = randint(5, Settings.window_height - self.rect.height - 100)
         self.speed_h = 0
         self.speed_v = 0
+
+    def movement(self,x,y):
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:        
+                if event.key == pygame.K_UP:    
+                    self.movey += y
+                if event.type == pygame.K_DOWN:
+                    self.movey -= y
+                if event.type == pygame.K_RIGHT:
+                    self.movex += x
+                if event.type == pygame.K_LEFT:
+                    self.movex -= x
+        # moving left
+        if self.movex < 0:
+            self.frame += 1
+            if self.frame > 3*ani:
+                self.frame = 0
+            self.image = self.images[self.frame//ani]
+
+        # moving right
+        if self.movex > 0:
+            self.frame += 1
+            if self.frame > 3*ani:
+                self.frame = 0
+            self.image = self.image[self.frame//ani]
+
+
 
     def update(self):
         if self.rect.left <= 0 or self.rect.right >= Settings.window_width:
@@ -44,6 +74,8 @@ class Player(pygame.sprite.Sprite):
         if self.rect.top <= 0 or self.rect.bottom >= Settings.window_height:
             self.change_direction_v()
         self.rect.move_ip((self.speed_h, self.speed_v))
+        self.rect.x = self.rect.x + self.movex
+        self.rect.y = self.rect.y + self.movey
 
     def draw(self, screen):
         screen.blit(self.image, self.rect)
@@ -96,7 +128,7 @@ class Game(object):
 
     def spawnplayer(self,num):
         for i in range(num):
-            self.player.add(Pacman("player.png"))
+            self.player.add(Player("player.png"))
 
     def spawnMeteors(self,num):
         for count in range(num):
@@ -104,22 +136,22 @@ class Game(object):
 
 
     def collide(self):
-        for pacman in self.player:
+        for player in self.player:
             for Meteors in self.Meteors:
                 self.radius = 25
-                if pygame.sprite.collide_circle(pacman,Meteors): 
+                if pygame.sprite.collide_circle(player,Meteors): 
                     self.Meteors.remove(Meteors)
 
     def delete_bottom(self):
-        for pacman in self.player:
+        for player in self.player:
             for Meteors in self.Meteors:
                     self.radius = 25
-                    if pygame.sprite.collide_circle(pacman,Meteors): 
+                    if pygame.sprite.collide_circle(player,Meteors): 
                         self.Meteors.remove(Meteors)
                         self.spawnMeteors(1)
     
     def run(self):
-        self.spawnplayer(5)
+        self.spawnplayer(1)
         self.spawnMeteors(10)
         while self.running:
             self.clock.tick(60)                         # Auf 1/60 Sekunde takten
